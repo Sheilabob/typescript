739435bad0205f771c8d2ab248cfd33608d91e83
Sheila Strahan <smstrahan@gmail.com>
2022-07-21 15:36:58 -0600

    lesson 4

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,20 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-character = 'luigi';
-age = 40;
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+var mixed = ['ken', 4, 'chun-li', 8, false];
+mixed.push('ryu');
+mixed.push(2);
+mixed.push(true);
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-console.log(circ(10));
+ninja.age = 40;
+ninja.name = 'ryu';
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
