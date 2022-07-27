57e7418fc2898b9b7e9cc9de007730d7516ca8ba
Sheila Strahan <smstrahan@gmail.com>
2022-07-27 17:50:59 -0600

    lesson 10

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,16 +1,25 @@
-type StringOrNum = string | number;
-type objWithName = { name: string, uid: StringOrNum };
+// let greet: Function;
 
+let greet: (a: string, b: string) => void;
 
-const logDetails = (uid: StringOrNum, item: string) => {
-    console.log(`${item} has a uid of ${uid}`);
+greet = (name: string, greeting: string) => {
+    console.log(`${name} says ${greeting}`);
 }
 
-const greet = (user: objWithName) => {
-    console.log(`${user.name} says hello`);
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+    if(action === 'add') {
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo;
+    }
 }
 
+let logDetails: (obj: {name: string, age: number}) => void;
 
-const greetAgain = (user: objWithName) => {
-    console.log(`${user.name} says hello`);
+type person = {name: string, age: number};
+
+logDetails = (ninja: person) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
 }
