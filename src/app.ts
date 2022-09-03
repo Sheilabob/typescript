9d878dab72901beb894d8b0ab4a7b3bebc11e3e9
Sheila Strahan <smstrahan@gmail.com>
2022-09-03 11:18:15 -0600

    typescript 20

--- src/app.ts
+++ src/app.ts
@@ -70,12 +70,15 @@ const list = new ListTemplate(ul);
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
+    let values: [string,string, number];
+    values = [tofrom.value, details.value, amount.valueAsNumber]
+
     let doc: Hasformatter;
 
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Invoice(...values)
     } else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Payment(...values)
     }
 
     console.log(
@@ -85,28 +88,19 @@ form.addEventListener('submit', (e: Event) => {
     list.render(doc, type.value, 'end');
 })
 
-//ENUMS
+// tuples
 
-enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
 
-interface Resource<T> {
-    uid: number;
-    resourceType: ResourceType;
-    data: T;
-}
+let tup: [string, number, boolean] = ['ryu', 25, true]
+tup[0] = 'ken';
+tup[1] = 30;
 
-const docOne: Resource<object> = {
-    uid: 1,
-    resourceType: ResourceType.BOOK,
-    data: { title: 'name of the wind' }
-}
+// let student: [string, number];
+// student = ['chun-li', 223423];
 
-const docTwo: Resource<object> = {
-    uid: 10,
-    resourceType: ResourceType.PERSON,
-    data: { name: 'yoshi' }
-}
-
-console.log(docOne, docTwo)
 
 
